4ef9b8d6fc9d29c43cdb1d5e8ad9f5d92a09386f
Steven Enke <stevenke73@gmail.com>
2014-10-17 20:17:10 -0500

    Finding K-th output

--- K-Value.cpp
+++ K-Value.cpp
@@ -5,11 +5,11 @@
 #include <algorithm>
 using namespace std;
 
-const MAX_N_NUMBERS = 100000;
+void countingSort(int numbersToSort[]m int numElements);
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-	int N, M, K;
+	int N, M, K, output;
 	vector<int> numbers;
 	
 	// First line of the input will contain three integers N, M and K separated by spaces respectively. 
@@ -19,10 +19,16 @@ int main() {
 	
 	// Second line of the input will contain N integers separated by spaces in clockwise order starting from an arbitrary location. 
 	numbers.resize (N);
-	for(int i = 0; i < N; i++) 
-	{
+	for(int i = 0; i < N; i++) {
 		cin >> numbers[i];
 	}
 	
+	// Sorting code in ascending order using sort function from algorith library
+	sort(numbers.begin(), numbers.end()); 
+	
+	// Finding K-th value_comp
+	output = numbers[K-1];
+	
+	cout << output;
     return 0;
 }
