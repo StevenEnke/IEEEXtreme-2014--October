4e7012bc0ba34117f646f414f41e7e91a2d1300c
Steven Enke <stevenke73@gmail.com>
2014-10-17 19:49:07 -0500

    Added method to get inputs

--- K-Value.cpp
+++ K-Value.cpp
@@ -5,8 +5,24 @@
 #include <algorithm>
 using namespace std;
 
+const MAX_N_NUMBERS = 100000;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+	int N, M, K;
+	vector<int> numbers;
+	
+	// First line of the input will contain three integers N, M and K separated by spaces respectively. 
+	cin >> N;
+	cin >> M;
+	cin >> K;
+	
+	// Second line of the input will contain N integers separated by spaces in clockwise order starting from an arbitrary location. 
+	numbers.resize (N);
+	for(int i = 0; i < N; i++) 
+	{
+		cin >> numbers[i];
+	}
+	
     return 0;
 }
