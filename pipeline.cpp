b67e65794cb56306fc7ae8ded94c0dd034ea2f27
Steven <stevenke73@gmail.com>
2014-10-18 00:57:05 -0500

    blah

--- pipeline.cpp
+++ pipeline.cpp
@@ -6,10 +6,11 @@
 using namespace std;
 const int INT_MAX=2147483647;
 int min(int x, int y, int z);
-int minCost(vector<vector<int> > cost, int m, int n, int Up);
+int minCost(vector<vector<int> > cost, int m, int n, int Up, int N);
 
 int main() {
-    int N, minMinCost = INT_MAX, currentMinCost;
+    int N;
+    int minMinCost = INT_MAX, currentMinCost;
     scanf("%d",&N);
     vector<vector<int> > cost;
     cost.resize(N);
@@ -26,7 +27,7 @@ int main() {
     }
     
     for(int i = 0; i < N; i++) {
-        currentMinCost = minCost(cost, i, N-1, 0);
+        currentMinCost = minCost(cost, i, N-1, 0, N);
 	cout<< currentMinCost <<endl;
 	if(minMinCost > currentMinCost) {
             minMinCost = currentMinCost;
@@ -37,28 +38,22 @@ int main() {
     return 0;
 }
 
-int minCost(vector<vector<int> > cost, int m, int n, int Up)
+int minCost(vector<vector<int> > cost, int m, int n, int Up, int N)
 {
-    if (n < 0 || m < 0 || n > 4 || m > 4)
-        return INT_MAX;
+    if (n < 0 || m < 0 || n > N-1 || m > N-1)
+        return INT_MAX/2;
     else if (m == 0 && n == 0)
         return cost[m][n];
     else {
-        cout << m << " " << n << endl;
-        cout << cost[m][n] << endl;
+//        cout << m << " " << n << endl;
+//        cout << cost[m][n] << endl;
 	// 0 allows up or down, 1 allows only up, 2 allows only down
-/*        if(Up == 0)
-            return cost[m][n] + min( minCost(cost, m-1, n, 2), minCost(cost, m, n-1, 0), minCost(cost, m+1, n, 1));
-        else if(Up == 1)
-            return cost[m][n] + min( minCost(cost, -1, -1, 0), minCost(cost, m, n-1, 0), minCost(cost, m+1, n, 1));
-        else // Up == 2
-	    return cost[m][n] + min( minCost(cost, -1, -1, 0), minCost(cost, m, n-1, 0), minCost(cost, m-1, n, 2));*/
         if(Up == 0)
-            return cost[m][n] + min( minCost(cost, m, n-1, 2), minCost(cost, m-1, n, 0), minCost(cost, m, n+1, 1));
+            return cost[m][n] + min( minCost(cost, m-1, n, 2, N), minCost(cost, m, n-1, 0, N), minCost(cost, m+1, n, 1, N));
         else if(Up == 1)
-            return cost[m][n] + min( minCost(cost, -1, -1, 0), minCost(cost, m-1, n, 0), minCost(cost, m, n+1, 1));
+            return cost[m][n] + min( minCost(cost, -1, -1, 0, N), minCost(cost, m, n-1, 0, N), minCost(cost, m+1, n, 1, N));
         else // Up == 2
-	    return cost[m][n] + min( minCost(cost, -1, -1, 0), minCost(cost, m-1, n, 0), minCost(cost, m, n-1, 2));
+	    return cost[m][n] + min( minCost(cost, -1, -1, 0, N), minCost(cost, m, n-1, 0, N), minCost(cost, m-1, n, 2, N));
     }
 }
 
